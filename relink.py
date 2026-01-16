9319a84d60463be204e696dad452814fa7c1ed9e
Sam Rabin <sam.rabin@gmail.com>
2026-01-15 17:12:01 -0700

    Revert "relink.py: Hide --source-root and --target-root args."
    I'll do something like this later...
    
    This reverts commit 06e97e29e23a30f33ea89ce0d2b2550c8c140a6a.

--- relink.py
+++ relink.py
@@ -188,20 +188,22 @@ def parse_arguments():
             "Find files owned by a user and replace them with symbolic links to a target directory."
         )
     )
-
-    # Source and target directories. Hide from help text because these should only be used for
-    # testing purposes. (That purpose could eventually be replaced with mocked default dirs.)
     parser.add_argument(
         "--source-root",
         type=validate_directory,
         default=DEFAULT_SOURCE_ROOT,
-        help=argparse.SUPPRESS,
+        help=(
+            f"The root of the directory tree to search for files (default: {DEFAULT_SOURCE_ROOT})"
+        ),
     )
     parser.add_argument(
         "--target-root",
         type=validate_directory,
         default=DEFAULT_TARGET_ROOT,
-        help=argparse.SUPPRESS,
+        help=(
+            f"The root of the directory tree where files should be moved to "
+            f"(default: {DEFAULT_TARGET_ROOT})"
+        ),
     )
 
     # Verbosity options (mutually exclusive)
